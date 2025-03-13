bdda63a377c758015ff7228b11202b0e0783a9f4
jackhmorgan <117677338+jackhmorgan@users.noreply.github.com>
2025-03-13 15:31:02 -0500

    Payoff calculator for n prisoners dilemma game

--- Quantum_Games/__init__.py
+++ Quantum_Games/__init__.py
@@ -1,6 +1,10 @@
 from .entanglement_layer import entanglement_layer
 from .player import player
 from .game_circuit import game_circuit
+from .get_aer_result import get_aer_result
+from .prisoners_dilemma_payoff_calculator import prisoners_dilemma_payoff_calculator
 __all__ = ['entanglement_layer',
            'player',
-           'game_circuit']
+           'game_circuit',
+           'get_aer_result',
+           'prisoners_dilemma_payoff_calculator']
---
+++ Quantum_Games/prisoners_dilemma_payoff_calculator.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+def prisoners_dilemma_payoff_calculator(results_df : pd.DataFrame):
+    payoff_df = pd.DataFrame(index=results_df.index)
+    n_players = results_df.shape[1]
+    for col in results_df:
+        payoff_df[col] = None
+    
+    for shot in results_df.iterrows():
+        n_long = np.count_nonzero(shot[1].values == 1)
+        for col_shot, col_payoff in zip(results_df, payoff_df):
+            if results_df[col_shot][shot[0]] == 0:
+                payoff_df.loc[shot[0], col_payoff] = 2*n_long/n_players
+            else:
+                payoff_df.loc[shot[0], col_payoff] = (2*n_long-n_players)/n_players
+
+    return payoff_df
